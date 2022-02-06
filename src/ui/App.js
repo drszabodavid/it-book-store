4137f2a96be04ee890dcb21ca269a30c2ce7cbfe
Dávid Szabó <david@revolin.io>
2022-02-06 17:41:05 +0100

    seperate MainPage compoennt

--- src/ui/App.js
+++ src/ui/App.js
@@ -1,34 +1,8 @@
-import React, { useEffect, useState } from "react";
-import useFetch from "../api/useFetch";
-import { SearchComponent } from "./Components";
-import { ErrorHandlingComponent } from "./Components/api";
+import React from "react";
+import { MainPage } from "./Pages";
 
 const App = () => {
-  const [bookData, setBookData] = useState([]);
-  const [initialBookData, setInitialBookData] = useState([]);
-  const { data, isLoading, error } = useFetch(
-    "https://api.itbook.store/1.0/new"
-  );
-
-  useEffect(() => {
-    if (!isLoading && data) {
-      const { books } = data;
-      setBookData(books);
-      setInitialBookData(books);
-    }
-  }, [isLoading, data]);
-
-  return (
-    <div className="flex h-screen w-full flex-col overflow-hidden bg-gray-500">
-      <SearchComponent
-        initialBookData={initialBookData}
-        setBookData={setBookData}
-      />
-      <ErrorHandlingComponent isLoading={isLoading} error={error} />
-      <h1>Featured Books</h1>
-      {!isLoading && bookData?.map(({ title }) => <div>{title}</div>)}
-    </div>
-  );
+  return <MainPage />;
 };
 
 export default App;
