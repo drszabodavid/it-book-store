fbd0fd07ad51d3622323b8352124680df77a1b6f
Dávid Szabó <david@revolin.io>
2022-02-06 17:41:43 +0100

    create error handling component

---
+++ src/ui/Components/APIComponents/LoadingComponent/LoadingComponent.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const LoadingComponent = () => {
+  return (
+    <div className="flex w-full mt-4 flex-col text-center text-blue-800 font-bolt">
+      I'm loading, please wait...
+    </div>
+  );
+};
+
+export default LoadingComponent;
