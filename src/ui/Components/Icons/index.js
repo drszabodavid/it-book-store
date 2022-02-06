c8a9ff67d596175a67a548378d8287c8708c81d6
Dávid Szabó <david@revolin.io>
2022-02-06 17:42:27 +0100

    add icons

--- src/ui/Components/Icons/index.js
+++ src/ui/Components/Icons/index.js
@@ -1,1 +1,4 @@
-export { default as SearchIcon } from "./SeachIcon/SearchIcon";
+export { default as SearchIcon } from "./SearchIcon/SearchIcon";
+export { default as JavaIcon } from "./JavaIcon/JavaIcon";
+export { default as JavascriptIcon } from "./JavascriptIcon/JavascriptIcon";
+export { default as PythonIcon } from "./PythonIcon/PythonIcon";
