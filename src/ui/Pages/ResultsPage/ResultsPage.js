3f34b9a3a6facf09ad5163e740c2b4e80aa88431
Dávid Szabó <david@revolin.io>
2022-02-14 20:19:20 +0100

    fix css

--- src/ui/Pages/ResultsPage/ResultsPage.js
+++ src/ui/Pages/ResultsPage/ResultsPage.js
@@ -4,12 +4,12 @@ import { ErrorHandlingComponent } from "../../Components/APIComponents";
 
 const ResultsPage = ({ searchData, isSearchLoading }) => {
   return (
-    <div className="flex flex-col w-screen h-full bg-leanderWeb">
+    <div className="flex flex-col w-full h-auto bg-leanderWeb no-scrollbar">
       <DividerText isSearch />
       {isSearchLoading ? (
         <ErrorHandlingComponent isLoading={isSearchLoading} />
       ) : (
-        <div className="flex flex-wrap self-center justify-center w-3/4">
+        <div className="flex flex-wrap self-center justify-center w-3/4 h-full">
           {searchData.map((book) => {
             return <BookCard book={book} />;
           })}
