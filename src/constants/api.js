b5f45d53ccbcd145654f8156cf5a0d6eabcbd587
Dávid Szabó <david@revolin.io>
2022-02-15 09:20:15 +0100

    fix url typo

--- src/constants/api.js
+++ src/constants/api.js
@@ -1,3 +1,3 @@
 export const minSearchCharacters = 3;
 export const featuredUrl = "https://api.itbook.store/1.0/new";
-export const searchUrl = "https://api.itbook.store/1.0/search/";
+export const searchUrl = "https://api.itbook.store/1.0/search";
