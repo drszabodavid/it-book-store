e8e60f3ad18aadf9e71e6e8ff75366ec888faf27
Dávid Szabó <david@revolin.io>
2022-02-15 17:29:32 +0100

    add key to categorybox

--- src/ui/Components/CategoryBox/CategoryBox.js
+++ src/ui/Components/CategoryBox/CategoryBox.js
@@ -3,7 +3,6 @@ import React from "react";
 const CategoryBox = ({ category, handleQuickSearch, quickSearch }) => {
   return (
     <div
-      key={category}
       onClick={() => handleQuickSearch(category)}
       className={`text-sx hover:bg-sandyBrown/50  mb-2 flex h-10 w-24 cursor-pointer items-center justify-center rounded-lg border-gray-500 bg-white shadow-sm md:mb-0 ${
         quickSearch === category && "bg-sandyBrown hover:bg-sandyBrown"
